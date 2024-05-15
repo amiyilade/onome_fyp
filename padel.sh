a179123c517781a44e39565a02c44fbfcbf2c197
blandashes <dawn1next@gmail.com>
2024-05-15 18:33:32 +0100

    edited lfs

--- padel.sh
+++ padel.sh
@@ -1,1 +1,3 @@
-java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv
+version https://git-lfs.github.com/spec/v1
+oid sha256:0b76896579fb462d710f165e0fc0a4de507ce6c0b16a32d8369c6b3b8710f94f
+size 231
